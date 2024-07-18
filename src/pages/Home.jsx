a738c67cd8c1abff68f38c73125306720aa53770
pranayram1630 <pranayramteke1630@gmail.com>
2024-07-18 17:40:39 +0530

    added-island-component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,30 @@ import { Suspense} from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 
+import  Island  from '../models/Island' 
+
 
 {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
         POPUP
         </div> */}
 
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+
+    if(window.innerWidth < 786 ){
+        screenScale = [0.9,0.9,0.9 ]
+        screenPosition = [0, -6.5, -43]
+    } else {
+      screenScale = [1,1,1];
+    }
+
+    return [screenScale , screenPosition, rotation]
+  }
+
+  const [islandScene, islandPosition, islandRotation] = adjustIslandForScreenSize();
   return (
     <section className='w-full h-screen relative'>
       
@@ -15,10 +33,19 @@ const Home = () => {
     camera={{ near:0.1, far: 1000 }}
     >
 
-    <Suspense fallback={<Loader />}/>
+    <Suspense fallback={<Loader />}>
+     <directionalLight/>
+     <pointLight/>
+     <spotLight/>
+     <hemisphereLight/>
+     <Island
+        position={islandPosition}
+        scale={islandScene}
+        rotation={islandRotation}
+     />
+    </Suspense> 
     </Canvas>
-
-    </section>
+  </section>
   )
 }
 
