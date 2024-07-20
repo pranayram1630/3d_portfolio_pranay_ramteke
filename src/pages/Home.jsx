8a23e144df832ed4fde470a544a599b2ea53d45f
pranayram1630 <pranayramteke1630@gmail.com>
2024-07-20 12:48:05 +0530

    added-sky

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 
 import  Island  from '../models/Island' 
+import Sky from '../models/Sky'
 
 
 {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
@@ -34,10 +35,12 @@ const Home = () => {
     >
 
     <Suspense fallback={<Loader />}>
-     <directionalLight/>
-     <pointLight/>
-     <spotLight/>
-     <hemisphereLight/>
+     <directionalLight position={[1,1,1]} intensity={2}/>
+     <ambientLight intensity={0.5}/>
+     <hemisphereLight skyColor='#b1e1ff' groundColor='#000000' intensity={1}/>
+
+    <Sky/>
+
      <Island
         position={islandPosition}
         scale={islandScene}
