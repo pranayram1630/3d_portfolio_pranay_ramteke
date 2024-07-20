4725ec1c5ad6834302478790d52be8c4deeccac8
pranayram1630 <pranayramteke1630@gmail.com>
2024-07-20 13:03:35 +0530

    added-bird-and-plane

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ import Loader from '../components/Loader'
 
 import  Island  from '../models/Island' 
 import Sky from '../models/Sky'
+import Bird from '../models/Bird'
+import Plane from '../models/Plane'
 
 
 {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
@@ -39,6 +41,7 @@ const Home = () => {
      <ambientLight intensity={0.5}/>
      <hemisphereLight skyColor='#b1e1ff' groundColor='#000000' intensity={1}/>
 
+    <Bird/>
     <Sky/>
 
      <Island
@@ -46,6 +49,7 @@ const Home = () => {
         scale={islandScene}
         rotation={islandRotation}
      />
+     <Plane/>
     </Suspense> 
     </Canvas>
   </section>
